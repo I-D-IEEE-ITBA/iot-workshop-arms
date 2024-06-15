3fd152298e6467d44fae4ef69c251429fa91b349
Ale Heir <aheir@itba.edu.ar>
2024-06-15 15:25:20 -0300

    add demo_offline project

---
+++ demo_offline/src/main.cpp
@@ -0,0 +1,224 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-06
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include <Arduino.h>
+#include <ESP32Servo.h>
+#include <FastLED.h>
+
+#include "debug.h"
+#include "pin_defines.h"
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+#define SMOOTH_TARGET 0.03 // multiplier for the target value on smoothing
+#define SMOOTH_PREV 0.97   // multiplier for the previous value on smoothing
+#define POS_TOLERANCE 0.5  // Tolerance for position, in degrees
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+typedef enum subs
+{
+    SUB_BASE, // Los 6 subs de los servos tienen que ser siempre los primeros 6
+    SUB_BASE_JOINT,
+    SUB_ELBOW,
+    SUB_WRIST_YAW,
+    SUB_WRIST_PITCH,
+    SUB_CLAW,
+    SUB_RGB,
+    SUB_LED_RED,
+    SUBSCRIPTIONS
+} SUBS_T;
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+static void setup_gpios(void);
+static void cycle_color(CRGB color);
+
+/*******************************************************************************
+ * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+/*******************************************************************************
+ * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+static Servo servos[6];
+
+// RGB strip
+static CRGB leds[NUM_LEDS];
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+void setup()
+{
+    Serial.begin(115200);
+
+    setup_gpios(); // initialize used GPIOS
+
+    pinMode(LED_RED, OUTPUT);
+    digitalWrite(LED_RED, LOW);
+
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
+
+    for (int i = 0; i < 6; i++)
+    {
+        // if(i == SUB_ELBOW) continue;
+        servos[i].setPeriodHertz(50); // standard 50 hz servo
+    }
+
+    servos[SUB_BASE].attach(BASE_SERVO_PIN, 1000, 2000);
+    servos[SUB_BASE_JOINT].attach(BASE_JOINT_SERVO_PIN, 1000, 2000);
+    servos[SUB_ELBOW].attach(ELBOW_SERVO_PIN, 1000, 2000);
+    servos[SUB_WRIST_YAW].attach(WRIST_YAW_SERVO_PIN, 1000, 2000);
+    servos[SUB_WRIST_PITCH].attach(WRIST_PITCH_SERVO_PIN, 1000, 2000);
+    servos[SUB_CLAW].attach(CLAW_SERVO_PIN, 1000, 2000);
+
+    FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(100);
+
+    digitalWrite(LED_RED, HIGH);
+    cycle_color(CRGB::Red);
+
+    for (int i = 0; i < 6; i++)
+    {
+        servos[i].write(90);
+        delay(250);
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        // if(i == SUB_ELBOW) continue;
+
+        Serial.print("\n\nTesteando servo: ");
+        Serial.println(i);
+        Serial.println("\n");
+
+        for (int j = 90; j < 135; j++)
+        {
+            servos[i].write(j);
+            delay(20);
+        }
+        for (int j = 135; j > 45; j--)
+        {
+            servos[i].write(j);
+            delay(20);
+        }
+        for (int j = 45; j < 90; j++)
+        {
+            servos[i].write(j);
+            delay(20);
+        }
+
+        delay(250);
+    }
+}
+
+void loop()
+{
+    digitalWrite(LED_RED, LOW);
+    cycle_color(CRGB::Black);
+    delay(200);
+    digitalWrite(LED_RED, HIGH);
+    cycle_color(CRGB::Red);
+    delay(200);
+
+    digitalWrite(LED_RED, LOW);
+    cycle_color(CRGB::Black);
+    delay(200);
+    digitalWrite(LED_RED, HIGH);
+    cycle_color(CRGB::Red);
+    delay(200);
+
+    digitalWrite(LED_RED, LOW);
+    cycle_color(CRGB::Black);
+    delay(200);
+    digitalWrite(LED_RED, HIGH);
+    cycle_color(CRGB::Red);
+    delay(200);
+
+    digitalWrite(LED_RED, LOW);
+    cycle_color(CRGB::Black);
+    delay(200);
+    digitalWrite(LED_RED, HIGH);
+    cycle_color(CRGB::Red);
+    delay(200);
+
+    for (int i = 0; i < 6; i++)
+    {
+
+        // if(i == SUB_ELBOW) continue;
+        Serial.print("\n\nTesteando servo: ");
+        Serial.println(i);
+        Serial.println("\n");
+
+        for (int j = 90; j < 135; j++)
+        {
+            servos[i].write(j);
+            delay(20);
+        }
+        for (int j = 135; j > 45; j--)
+        {
+            servos[i].write(j);
+            delay(20);
+        }
+        for (int j = 45; j < 90; j++)
+        {
+            servos[i].write(j);
+            delay(20);
+        }
+
+        delay(250);
+    }
+}
+
+/*******************************************************************************
+ *******************************************************************************
+                        LOCAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+static void setup_gpios(void)
+{
+    pinMode(Board_LED, OUTPUT);
+    digitalWrite(Board_LED, Board_LED_OFF);
+    pinMode(External_LED, OUTPUT);
+    digitalWrite(External_LED, External_LED_OFF);
+}
+
+static void cycle_color(CRGB color)
+{
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = CRGB::Black;
+        FastLED.show();
+        delay(50);
+        leds[i] = color;
+        FastLED.show();
+    }
+    FastLED.show();
+}
